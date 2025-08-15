64347a3a0d4cf2bd147acd5538532eb59f6f15d5
iamchandupa <silvachandupa@gmail.com>
2025-08-15 19:38:50 +0530

    Edited css

---
+++ typing.js
@@ -0,0 +1,31 @@
+const words = ["UI Designer", "Designer", "Undergraduate Student"];
+const typingSpeed = 150;  
+const erasingSpeed = 100; 
+const delayBetween = 1000;
+
+let wordIndex = 0;
+let charIndex = 0;
+const typingElement = document.querySelector(".typing");
+
+function type() {
+  if (charIndex < words[wordIndex].length) {
+    typingElement.textContent += words[wordIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingSpeed);
+  } else {
+    setTimeout(erase, delayBetween);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingElement.textContent = words[wordIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingSpeed);
+  } else {
+    wordIndex = (wordIndex + 1) % words.length;
+    setTimeout(type, typingSpeed);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", type);
